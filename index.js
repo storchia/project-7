d1c0972d6898906254450ecfeec201af6e603a95
Sergio Andres <sergiotorchia@hotmail.com>
2022-07-05 20:02:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// - Un nuevo Set con los nombres de tu familia
+const miSet = new Set(['Juan', 'Sergio', 'Carlos', 'Pedro', 'Jose']);
+
+// - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+miSet.add('Sergio');
+
+// - Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+miSet.add('Javascript');
+console.log(miSet);
